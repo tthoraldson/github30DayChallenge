c5286a1edaa15c92fe0540e6427fd3fb53869ea0
Drew Wiskus <andrewwiskus@gmail.com>
2016-09-24 14:27:41 -0500

    added db structure in database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+ id SERIAL PRIMARY KEY,
+ github_url varchar(50),
+ email varchar(50),
+ display_name varchar(200),
+ authToken varchar(255),
+ user_id varchar(20),
+ profile_photo varchar(255),
+ auth_level varchar(100)
+);
