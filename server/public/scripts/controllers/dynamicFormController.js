8ed959e15a5472642491018cf3801046cd9700ad
Drew Wiskus <andrewwiskus@gmail.com>
2016-09-27 09:33:10 -0500

    messed with the dynamic form

--- server/public/scripts/controllers/dynamicFormController.js
+++ server/public/scripts/controllers/dynamicFormController.js
@@ -1,49 +1,67 @@
 myApp.controller("DynamicFormController", ["$scope", "$http", "$location", 'AuthFactory', function($scope, $http, $location, AuthFactory) {
-  console.log("DynamicFormController works");
+    console.log("DynamicFormController works");
 
-  //
-  $scope.surveyTitle = {
-    title: "Untitled Survey",
-    descriptino: ""
-  };
+    //
+    $scope.surveyTitle = {
+        formDBref: 0,
+        title: "Untitled Survey",
+        descriptino: ""
+    };
 
-  //
-  $scope.surveyQuestions = [{
-    title: "untitled question",
-    multipleChoice: [{check: false, option: "yes"},{check: false, option: "no"},{check: false, option: "maybe"}, {check: false, option: "so"}],
-    checkbox: [{check: false, option: "eggs"}, {check: false, option: "sushi"}, {check: false, option: "kalamari"},{check: false, option: "corn"}],
-    shortAnswer: ""
-  }];
+    $scope.surveyQuestions = [{
+        title: "untitled question",
+        multipleChoice: [{
+            check: false,
+            option: "untitled option"
+        }],
+        checkbox: [{
+            check: false,
+            option: "untitled option"
+        }],
+        shortAnswer: ""
+    }];
 
-  //
-  $scope.shortAnswer_selected = false;
-  $scope.multipleChoice_selected = false;
-  $scope.checkBox_selected = false;
 
 
-  //
-  function resetForm(){
-  $scope.shortAnswer_selected = false;
-  $scope.multipleChoice_selected = false;
-  $scope.checkBox_selected = false;
-  }
+    //
+    $scope.shortAnswer_selected = false;
+    $scope.multipleChoice_selected = false;
+    $scope.checkBox_selected = false;
 
-  //
-  $scope.selectedDataType = function(dataType){
-    resetForm();
-    console.log('DATA:', dataType)
-    switch(dataType){
-      case 'shortAnswer':
-          $scope.shortAnswer_selected = true;
-      break;
-      case 'multipleChoice':
-          $scope.multipleChoice_selected = true;
-      break;
-      case 'checkBox':
-          $scope.checkBox_selected = true;
-      break;
+
+    // resets displayed form on button push
+    function resetForm() {
+        $scope.shortAnswer_selected = false;
+        $scope.multipleChoice_selected = false;
+        $scope.checkBox_selected = false;
+    }
+
+    // handles form views for different question types
+    $scope.selectedDataType = function(dataType) {
+        resetForm();
+        console.log('DATA:', dataType)
+        switch (dataType) {
+            case 'shortAnswer':
+                $scope.shortAnswer_selected = true;
+                break;
+            case 'multipleChoice':
+                $scope.multipleChoice_selected = true;
+                break;
+            case 'checkBox':
+                $scope.checkBox_selected = true;
+                break;
+
+        }
+    }
+
+    $scope.addOption = function(){
+      if($scope.checkBox_selected){
+        console.log('add a checkbox option')
+      }
+      if($scope.multipleChoice_selected){
+        console.log('add a multiC option')
+      }
+
 
     }
-  }
-
 }]);
