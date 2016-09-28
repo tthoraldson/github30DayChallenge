5eafbd2eda31b7a8891dfc42388a90496db62399
twinklefingers <rachelweila@gmail.com>
2016-09-28 16:09:05 -0500

    TWO DATAS

--- server/public/scripts/controllers/dataPageController.js
+++ server/public/scripts/controllers/dataPageController.js
@@ -1,8 +1,6 @@
 myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFactory', function($scope, $http, $location, AuthFactory) {
     console.log("Loaded: Data Page Controller");
 
-
-
     // function BUILDSPRINT2DATABASE() {
     //     $http.put('/databaseBuild').then(function() {});
     // }
@@ -35,166 +33,193 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
     }
 
     getData('sprint2').then(function(data) {
-            $scope.sprintHistory[1] = {
-                title: "Sprint 2 (08/23-09/21)",
-                data: data
-            };
+        $scope.sprintHistory[1] = {
+            title: "Sprint 2 (08/23-09/21)",
+            data: data
+        };
 
-            // the following code gets just Liz's 0/1 for 8/23
-            // dayData = $scope.sprintHistory[1].data[0].date_8_23;
+        // the following code gets just Liz's 0/1 for 8/23
+        // dayData = $scope.sprintHistory[1].data[0].date_8_23;
 
-            var playerData = $scope.sprintHistory[1].data;
-            var count = {};
 
-            for (i = 1; i < playerData.length; i++) {
-                var day = playerData[i];
 
-                _.keys(day).forEach(function(key) {
-                    if (key == "member_score" || key == "id" || key == "member_name" || key == "member_team" || key == "team_score") {
+        var playerData = $scope.sprintHistory[1].data;
+        var count = {};
 
-                    } else {
-                        count[key] = 0;
+        for (i = 1; i < playerData.length; i++) {
+            var day = playerData[i];
+
+            _.keys(day).forEach(function(key) {
+                if (key == "member_score" || key == "id" || key == "member_name" || key == "member_team" || key == "team_score") {
+
+                } else {
+                    count[key] = 0;
+                }
+            }); // end for each
+        }
+        for (i = 1; i < playerData.length; i++) {
+            var day = playerData[i];
+
+            _.keys(day).forEach(function(key) {
+
+                if (key == "member_score" || key == "id" || key == "member_name" || key == "member_team" || key == "team_score") {
+                    // do nothing
+                } else {
+                    if (day[key] == 1) {
+                        count[key]++;
                     }
-                }); // end for each
-            }
-            for (i = 1; i < playerData.length; i++) {
-                var day = playerData[i];
+                }
+            }); // end for each
 
-                _.keys(day).forEach(function(key) {
-
-                    if (key == "member_score" || key == "id" || key == "member_name" || key == "member_team" || key == "team_score") {
-
-                    } else {
-                        if (day[key] == 1) {
-                            count[key]++;
-                        }
-                    }
-                }); // end for each
-
-                var data = [];
-                _.keys(count).forEach(function(key, i) {
-                    data.push({
-                        'day': i,
-                        "commit": count[key]
-                    })
-                });
-            }
-
-            //============================== d3 ===========================//
-            // Set the dimensions of the canvas / graph
-            var margin = {
-                    top: 30,
-                    right: 20,
-                    bottom: 30,
-                    left: 50
-                },
-                width = 750 - margin.left - margin.right,
-                height = 250 - margin.top - margin.bottom;
-
-            // Parse the date / time
-            // var	parseDate = d3.time.format("%d-%b-%y").parse;
-            // var formatTime = d3.time.format("%e %B");// Format tooltip date / time
-
-            // Set the ranges
-            var x = d3.scale.linear().range([0, width]);
-            var y = d3.scale.linear().range([height, 0]);
-
-
-            // Define the axes
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient("bottom").ticks(10);
-
-            var yAxis = d3.svg.axis()
-                .scale(y)
-                .orient("left").ticks(2);
-
-            // Define the line
-            var valueline = d3.svg.line()
-                .x(function(d) {
-                    return x(d.day);
+            var data = [];
+            _.keys(count).forEach(function(key, i) {
+                data.push({
+                    'day': i,
+                    'commit': count[key]
                 })
-                .y(function(d) {
-                    return y(d.commit);
-                });
+            });
+        }
 
-            // Define 'div' for tooltips
-            var div = d3.select("#chart")
-                .append("div") // declare the tooltip div
-                .attr("class", "tooltip") // apply the 'tooltip' class
-                .style("opacity", 0); // set the opacity to nil
+        //============================== d3 ===========================//
+        // Set the dimensions of the canvas / graph
+        var margin = {
+                top: 30,
+                right: 20,
+                bottom: 30,
+                left: 50
+            },
+            width = 750 - margin.left - margin.right,
+            height = 250 - margin.top - margin.bottom;
 
-            // Adds the svg canvas
-            var svg = d3.select("#chart")
-                .append("svg")
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform",
-                    "translate(" + margin.left + "," + margin.top + ")");
+        // Parse the date / time
+        // var	parseDate = d3.time.format("%d-%b-%y").parse;
+        // var formatTime = d3.time.format("%e %B");// Format tooltip date / time
 
-            // Get the data
-            //d3.csv("data.csv", function(error, data) {
+        // Set the ranges
+        var x = d3.scale.linear().range([0, width]);
+        var y = d3.scale.linear().range([height, 0]);
 
-            // data.forEach(function(d) {
-            // 	d.day = parseDate(d.day);
-            // 	d.commit = +d.commit;
-            // });
 
-            // Scale the range of the data
-            x.domain(d3.extent(data, function(d) {
-                return d.day;
-            }));
-            y.domain([0, d3.max(data, function(d) {
-                return d.commit;
-            })]);
+        // Define the axes
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom").ticks(10);
 
-            // Add the valueline path.
-            svg.append("path")
-                .attr("class", "line")
-                .attr("d", valueline(data));
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left").ticks(2);
 
-            // draw the scatterplot
-            svg.selectAll("dot")
-                .data(data)
-                .enter().append("circle")
-                .attr("r", 3)
-                .attr("cx", function(d) {
-                    return x(d.day);
-                })
-                .attr("cy", function(d) {
-                    return y(d.commit);
-                })
+        // Define the line
+        var valueline = d3.svg.line()
+            .x(function(d) {
+                return x(d.day);
+            })
+            .y(function(d) {
+                return y(d.commit);
+            });
 
-            // Tooltip stuff after this
-            // .on("mouseover", function(d) {
-            // div.transition()
-            // 	.duration(500)
-            // 	.style("opacity", 0);
-            // div.transition()
-            // 	.duration(200)
-            // 	.style("opacity", .9);
-            // div	.html(
-            // 	'<a>' + // The first <a> tag
-            // 	formatTime(d.day) +
-            // 	"</a>" +    // closing </a> tag
-            // 	"<br/>"  + d.commit)
-            // .style("left", (d3.event.pageX) + "px")
-            // .style("top", (d3.event.pageY - 28) + "px");
-            // });
+        // // Define 'div' for tooltips
+        // var div = d3.select("#chart")
+        //     .append("div") // declare the tooltip div
+        //     // .attr("class", "tooltip") // apply the 'tooltip' class
+        //     .style("opacity", 0); // set the opacity to nil
 
-            // Add the X Axis
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
+        // Adds the svg canvas
+        var svg = d3.select("#chart")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
 
-            // Add the Y Axis
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis);
+        var data2 = [
+            {"day": 1, 'commit': 30},
+            {'day': 2, 'commit': 50},
+            {'day': 3, 'commit': 80},
+            {'day': 4, 'commit': 60},
+            {'day': 5, 'commit': 70},
+            {'day': 6, 'commit': 30},
+            {'day': 7, 'commit': 90},
+            {'day': 8, 'commit': 100},
+            {'day': 9, 'commit': 20},
+            {'day': 10, 'commit': 10}
+          ];
 
-        }) // end getData promise
+        // // yo yo this is the function that will produce multiple lines
+        // var dataNest = d3.nest()
+        //     .key(function(d) {
+        //         return d.symbol; // idk wtf .symbol is doing :(
+        //     })
+        //     .entries(data); // fucking entries wtf!!??
+
+        // Get the data
+        //d3.csv("data.csv", function(error, data) {
+
+        // data.forEach(function(d) {
+        // 	d.day = parseDate(d.day);
+        // 	d.commit = +d.commit;
+        // });
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function(d) {
+            return d.day;
+        }));
+        y.domain([0, d3.max(data2, function(d) {
+            return d.commit;
+        })]);
+
+
+        var dataArray = [data, data2];
+        // Add the valueline path.
+        dataArray.forEach (function(data){
+
+          svg.append("path")
+              .attr("class", "line")
+              .attr("d", valueline(data))
+
+              // draw the scatterplot
+              svg.selectAll("dot")
+                  .data(data)
+                  .enter().append("circle")
+                  .attr("r", 3)
+                  .attr("cx", function(d) {
+                      return x(d.day);
+                  })
+                  .attr("cy", function(d) {
+                      return y(d.commit);
+                  })
+        })
+
+        // Tooltip stuff after this
+        // .on("mouseover", function(d) {
+        // div.transition()
+        // 	.duration(500)
+        // 	.style("opacity", 0);
+        // div.transition()
+        // 	.duration(200)
+        // 	.style("opacity", .9);
+        // div	.html(
+        // 	'<a>' + // The first <a> tag
+        // 	formatTime(d.day) +
+        // 	"</a>" +    // closing </a> tag
+        // 	"<br/>"  + d.commit)
+        // .style("left", (d3.event.pageX) + "px")
+        // .style("top", (d3.event.pageY - 28) + "px");
+        // });
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+    }) // end getData promise
 
 
     $scope.clickedSprint = function(sprintTitle) {
