30894d7390533e959baac77f36cdf2c1afa0acd8
twinklefingers <rachelweila@gmail.com>
2016-10-05 10:58:22 -0500

    pulling again

--- server/public/scripts/controllers/dataPageController.js
+++ server/public/scripts/controllers/dataPageController.js
@@ -94,17 +94,12 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
                       sprint2: 'meto'
                     }
 
-
-
               lData.forEach(function(commit){
 
                 if (user.github_url.substring(19) == commit.github){
                   tempLawn.push({date: commit.date, commits: commit.commits})
                 }
-
-
               })
-
               tempArray.push(new User(id, auth_level, display_name, email, github_url, profile_photo, user_id, language, team, tempLawn, sprintHistory))
               tempLawn = [];
             });
@@ -114,12 +109,7 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
             console.log('All info on current members in database', $scope.userData);
 
           })
-
-
         });
-
-
-
       }
 
       buildUserData();
@@ -144,8 +134,6 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
     //
     //
     // }
-
-
     // dataForEachMember('user_lawns')
 
 
@@ -162,6 +150,9 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
 
 
 
+    ///////////////////////////////////////
+    //           D3 DATA CODE            //
+    ///////////////////////////////////////
     getData('sprint2').then(function(data) {
         $scope.sprintHistory[1] = {
             title: "Sprint 2 (08/23-09/21)",
@@ -208,6 +199,22 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
             });
         }
 
+        var data2 = [
+            {"day": 1, 'commit': 30},
+            {'day': 2, 'commit': 50},
+            {'day': 3, 'commit': 80},
+            {'day': 4, 'commit': 60},
+            {'day': 5, 'commit': 70},
+            {'day': 6, 'commit': 30},
+            {'day': 7, 'commit': 90},
+            {'day': 8, 'commit': 100},
+            {'day': 9, 'commit': 20},
+            {'day': 10, 'commit': 10}
+          ];
+
+          var dataArray = [data, data2];
+
+
         //============================== d3 ===========================//
         // Set the dimensions of the canvas / graph
                 var margin = {
@@ -218,10 +225,6 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
                     },
                     width = 750 - margin.left - margin.right,
                     height = 250 - margin.top - margin.bottom;
-
-                // Parse the date / time
-                // var	parseDate = d3.time.format("%d-%b-%y").parse;
-                // var formatTime = d3.time.format("%e %B");// Format tooltip date / time
 
                 // Set the ranges
                 var x = d3.scale.linear().range([0, width]);
@@ -246,12 +249,6 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
                         return y(d.commit);
                     });
 
-                // // Define 'div' for tooltips
-                // var div = d3.select("#chart")
-                //     .append("div") // declare the tooltip div
-                //     // .attr("class", "tooltip") // apply the 'tooltip' class
-                //     .style("opacity", 0); // set the opacity to nil
-
                 // Adds the svg canvas
                 var svg = d3.select("#chart")
                     .append("svg")
@@ -261,34 +258,6 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
                     .attr("transform",
                         "translate(" + margin.left + "," + margin.top + ")");
 
-                var data2 = [
-                    {"day": 1, 'commit': 30},
-                    {'day': 2, 'commit': 50},
-                    {'day': 3, 'commit': 80},
-                    {'day': 4, 'commit': 60},
-                    {'day': 5, 'commit': 70},
-                    {'day': 6, 'commit': 30},
-                    {'day': 7, 'commit': 90},
-                    {'day': 8, 'commit': 100},
-                    {'day': 9, 'commit': 20},
-                    {'day': 10, 'commit': 10}
-                  ];
-
-                // // yo yo this is the function that will produce multiple lines
-                // var dataNest = d3.nest()
-                //     .key(function(d) {
-                //         return d.symbol; // idk wtf .symbol is doing :(
-                //     })
-                //     .entries(data); // fucking entries wtf!!??
-
-                // Get the data
-                //d3.csv("data.csv", function(error, data) {
-
-                // data.forEach(function(d) {
-                // 	d.day = parseDate(d.day);
-                // 	d.commit = +d.commit;
-                // });
-
                 // Scale the range of the data
                 x.domain(d3.extent(data, function(d) {
                     return d.day;
@@ -297,12 +266,6 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
                 y.domain([0, d3.max(data2, function(d) {
                     return d.commit;
                 })]);
-
-
-                var dataArray = [data, data2];
-
-
-
 
 
                 ///////////////////////////////////////
@@ -347,34 +310,6 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
 
                 var color = d3.scale.category10();
 
-                // Loop through each symbol / key
-                // dataArray.forEach(function(d) {
-                //
-                //     svg.append("path")
-                //         .attr("class", "line")
-                //         .style("stroke", function() {
-                //             return d.color = color(d.key); })
-                //         .attr("d", valueline(d.values));
-                //
-                // });
-
-                // Tooltip stuff after this
-                // .on("mouseover", function(d) {
-                // div.transition()
-                // 	.duration(500)
-                // 	.style("opacity", 0);
-                // div.transition()
-                // 	.duration(200)
-                // 	.style("opacity", .9);
-                // div	.html(
-                // 	'<a>' + // The first <a> tag
-                // 	formatTime(d.day) +
-                // 	"</a>" +    // closing </a> tag
-                // 	"<br/>"  + d.commit)
-                // .style("left", (d3.event.pageX) + "px")
-                // .style("top", (d3.event.pageY - 28) + "px");
-                // });
-
                 // Add the X Axis
                 svg.append("g")
                     .attr("class", "x axis")
@@ -389,193 +324,6 @@ myApp.controller("DataPageController", ["$scope", "$http", "$location", 'AuthFac
             }) // end getData promise
 
 
-// experiments
-// Set the dimensions of the canvas / graph
-// var margin = {
-//         top: 30,
-//         right: 20,
-//         bottom: 30,
-//         left: 50
-//     },
-//     width = 750 - margin.left - margin.right,
-//     height = 250 - margin.top - margin.bottom;
-//
-// // Parse the date / time
-// // var	parseDate = d3.time.format("%d-%b-%y").parse;
-// // var formatTime = d3.time.format("%e %B");// Format tooltip date / time
-//
-// // Set the ranges
-// var x = d3.scale.linear().range([0, width]);
-// var y = d3.scale.linear().range([height, 0]);
-//
-//
-// // Define the axes
-// var xAxis = d3.svg.axis()
-//     .scale(x)
-//     .orient("bottom").ticks(10);
-//
-// var yAxis = d3.svg.axis()
-//     .scale(y)
-//     .orient("left").ticks(2);
-//
-// // Define the line
-// var valueline = d3.svg.line()
-//     .x(function(d) {
-//         return x(d.day);
-//     })
-//     .y(function(d) {
-//         return y(d.commit);
-//     });
-//
-// // // Define 'div' for tooltips
-// // var div = d3.select("#chart")
-// //     .append("div") // declare the tooltip div
-// //     // .attr("class", "tooltip") // apply the 'tooltip' class
-// //     .style("opacity", 0); // set the opacity to nil
-//
-// // Adds the svg canvas
-// var svg = d3.select("#chart")
-//     .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//     .append("g")
-//     .attr("transform",
-//         "translate(" + margin.left + "," + margin.top + ")");
-//
-// var data2 = [
-//     {"day": 1, 'commit': 30},
-//     {'day': 2, 'commit': 50},
-//     {'day': 3, 'commit': 80},
-//     {'day': 4, 'commit': 60},
-//     {'day': 5, 'commit': 70},
-//     {'day': 6, 'commit': 30},
-//     {'day': 7, 'commit': 90},
-//     {'day': 8, 'commit': 100},
-//     {'day': 9, 'commit': 20},
-//     {'day': 10, 'commit': 10}
-//   ];
-//
-//   var dataArray = [data, data2];
-//   // Add the valueline path.
-//   // dataArray.forEach (function(data){
-//   //
-//   //   svg.append("path")
-//   //       .attr("class", "line")
-//   //       .attr("d", valueline(data))
-//   //
-//   //       // draw the scatterplot
-//   //       svg.selectAll("dot")
-//   //           .data(data)
-//   //           .enter().append("circle")
-//   //           .attr("r", 3)
-//   //           .attr("cx", function(d) {
-//   //               return x(d.day);
-//   //           })
-//   //           .attr("cy", function(d) {
-//   //               return y(d.commit);
-//   //           })
-//   // })
-//
-// // // yo yo this is the function that will produce multiple lines
-// var dataNest = d3.nest()
-//     .key(function(d) {
-//         return d.symbol; // idk wtf .symbol is doing :(
-//     })
-//     .entries(dataArray); // fucking entries wtf!!??
-//
-//
-// var color = d3.scale.category10();
-//
-// legendSpace = width/dataNest.length; // spacing for the legend
-//
-//     dataNest.forEach (function(data){
-//       svg.append("path")
-//           .attr("class", "line")
-//           .attr("d", valueline(data))
-//
-//           // draw the scatterplot
-//           svg.selectAll("dot")
-//               .data(data)
-//               .enter().append("circle")
-//               .attr("r", 3)
-//               .attr("cx", function(d) {
-//                   return x(data.day);
-//               })
-//               .attr("cy", function(d) {
-//                   return y(data.commit);
-//               })
-//
-//               // Add the Legend
-//             svg.append("text")
-//                 .attr("x", (legendSpace/2)+i*legendSpace)  // space legend
-//                 .attr("y", height + (margin.bottom/2)+ 5)
-//                 .attr("class", "legend")    // style the legend
-//                 .style("fill", function() { // Add the colours dynamically
-//                     return data.color = color(data.key); })
-//                 .on("click", function(){
-//                     // Determine if current line is visible
-//                     var active   = data.active ? false : true,
-//                     newOpacity = active ? 0 : 1;
-//                     // Hide or show the elements based on the ID
-//                     d3.select("#tag"+data.key.replace(/\s+/g, ''))
-//                         .transition().duration(100)
-//                         .style("opacity", newOpacity);
-//                     // Update whether or not the elements are active
-//                     data.active = active;
-//                     })
-//                 .text(data.key);
-//     })
-//
-// // Scale the range of the data
-// x.domain(d3.extent(data, function(d) {
-//     return d.day;
-// }));
-// // be sure these are set to create the x/y axises based on the largest data set
-// y.domain([0, d3.max(data2, function(d) {
-//     return d.commit;
-// })]);
-//
-//
-// // Loop through each symbol / key
-// dataArray.forEach(function(d) {
-//
-//     svg.append("path")
-//         .attr("class", "line")
-//         .style("stroke", function() {
-//             return d.color = color(d.key); })
-//         .attr("d", valueline(d.values));
-//
-// });
-//
-// // Tooltip stuff after this
-// // .on("mouseover", function(d) {
-// // div.transition()
-// // 	.duration(500)
-// // 	.style("opacity", 0);
-// // div.transition()
-// // 	.duration(200)
-// // 	.style("opacity", .9);
-// // div	.html(
-// // 	'<a>' + // The first <a> tag
-// // 	formatTime(d.day) +
-// // 	"</a>" +    // closing </a> tag
-// // 	"<br/>"  + d.commit)
-// // .style("left", (d3.event.pageX) + "px")
-// // .style("top", (d3.event.pageY - 28) + "px");
-// // });
-//
-// // Add the X Axis
-// svg.append("g")
-//     .attr("class", "x axis")
-//     .attr("transform", "translate(0," + height + ")")
-//     .call(xAxis);
-//
-// // Add the Y Axis
-// svg.append("g")
-//     .attr("class", "y axis")
-//     .call(yAxis);
-//
-// }) // end getData promise
 
     $scope.clickedSprint = function(sprintTitle) {
         console.log('clicked on: ', sprintTitle);
