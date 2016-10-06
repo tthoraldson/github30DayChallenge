9db2ae01f9b741e8fa9125dcab7e02d7d99a9e0a
Drew Wiskus <andrewwiskus@gmail.com>
2016-10-06 00:20:01 -0500

    hardcodes

--- server/public/scripts/controllers/inviteController.js
+++ server/public/scripts/controllers/inviteController.js
@@ -1,6 +1,10 @@
 myApp.controller("InviteController", ["$scope", "$http", "$location", "AuthFactory", "UserFactory", "EmailFactory", function($scope, $http, $location, AuthFactory, UserFactory, EmailFactory) {
     console.log("InviteController works");
+    $scope.results = false;
+    $scope.question1 = false;
+    $scope.question2 = false;
 
+    
     $scope.auth = AuthFactory;
     $scope.emailInfo= {};
 
